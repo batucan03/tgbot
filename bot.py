a014ab400ceddc0f27be8b23d6874eca83cc2cbb
batucan03 <batucanxy@gmail.com>
2025-04-03 22:59:28 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+# telegram/bot.py
+from telegram.ext import Updater
+from config import TOKEN
+from telegram.commands import register_commands
+from telegram.buttons import register_buttons
+from telegram.messages import register_messages
+from telegram.admin import register_admin_buttons
+
+def start_bot():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    register_commands(dp)
+    register_buttons(dp)
+    register_messages(dp)
+    register_admin_buttons(dp)
+    updater.start_polling()
+    updater.idle()
